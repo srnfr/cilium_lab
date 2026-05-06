b52a8820079a499c60fb8c4035816e0fd2bf1f5a
Stephane <srnfr@users.noreply.github.com>
2026-05-06 15:24:49 +0200

    Fix tar command execution and improve readability

--- basic/00-build-foundation.sh
+++ basic/00-build-foundation.sh
@@ -26,7 +26,8 @@ OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/' -e 's/aarch64$/arm64/')"
 KREW="krew-${OS}_${ARCH}"
 curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz"
-tar zxvf "${KREW}.tar.gz" && ./"${KREW}" install krew)
+tar zxvf "${KREW}.tar.gz"
+./"${KREW}" install krew
 echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
 cd /home/cilium_lab
 
